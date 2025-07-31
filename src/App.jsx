c6822862cc3df930698599c262717a0e5c62eb7c
Suyashnichit308 <nichitsuyash@gmail.com>
2025-07-31 11:28:34 +0530

    hero section completed

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Header from './components/header/header'
+import Hero from './components/hero-section/Hero'
+import Marquee from './components/marquee/Marquee'
+
+
+function App() {
+  
+
+  return (<>
+  
+    <Header/>
+    <Hero/>
+    <Marquee/>
+ 
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/header.jsx
@@ -0,0 +1,32 @@
+import './header.css'
+import PrimaryBtn from './PrimaryBtn';
+import dukaanLogo from "/logo.svg"
+
+
+function Header() {
+    const links=["Product","Company","Resource","Pricing"]
+  return (
+    <header>
+        <div className='header-left'>
+            <a href="/">
+            <img src={dukaanLogo} alt="dukaan logo"  className='logo'/>
+            </a>
+            <ul>
+                {links.map((link)=>{
+                    return ( <li>
+                    <a href="/">{link}</a>
+                </li>
+                );
+                })}
+            </ul>
+        </div>
+        <div className="header-right">
+            <a href="/">Sign in</a>
+           <PrimaryBtn title={"Start free"}/>
+        </div>
+    </header>
+
+  )
+}
+
+export default Header
