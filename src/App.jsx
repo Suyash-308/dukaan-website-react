29bf52c17f98084324cf2b125127786afce5c5d9
Suyashnichit308 <nichitsuyash@gmail.com>
2025-08-12 22:10:25 +0530

    added from dukaan

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Dukaan from './components/dukaan/Dukaan'
 import Header from './components/Header/Header'
 import Hero from './components/hero-section/Hero'
 import Marquee from './components/marquee/Marquee'
-import ECommerce from './components/section-E commerce/ECommerce'
+import ECommerce from './components/section-E-commerce/ECommerce'
 
 
 function App() {
