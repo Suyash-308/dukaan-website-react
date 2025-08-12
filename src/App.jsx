fa200ff01299fdb8358b2262d83c29436109f91b
Suyashnichit308 <nichitsuyash@gmail.com>
2025-08-12 22:48:34 +0530

    added from dukaan

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Dukaan from './components/dukaan/Dukaan'
+import Dukaan from './components/dukaan/Dukaan'           
 import Header from './components/Header/Header'
 import Hero from './components/hero-section/Hero'
 import Marquee from './components/marquee/Marquee'
@@ -7,19 +7,16 @@ import ECommerce from './components/section-E-commerce/ECommerce'
 
 
 function App() {
-  
-
-  return (<>
-  
-    <Header/>
-    <Hero/>
-    <Marquee/>
-    <Dukaan/>
-    <ECommerce/>
- 
-
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Marquee />
+      <Dukaan />
+      <ECommerce />
     </>
-  )
+  );
 }
+
 
 export default App
